18baff08f6829df689788ecc20a1557e80a6f7c0
hyunseo <80755341+hyunseo1206@users.noreply.github.com>
2022-10-17 12:11:39 +0900

    Create pthread_create.c

---
+++ pthread/pthread_create.c
@@ -0,0 +1,19 @@
+int pthread_create(pthread_t * restrict thread,
+		const pthread_attr_t *restrict attr,
+		void *(*start_routine)(void *),
+		void *restrict arg);
+    
+    
+/*    
+pthread_t *restrict thread
+스레드의 식별자
+    
+const pthread_attr_t *restrict attr
+설정할 스레드 속성정보(NULL 입력시 기본설정 적용)
+    
+void *(*start_routine)(void *)
+스레드가 실행할 함수
+    
+void *restrict arg
+스레드가 실행할 함수에 들어갈 인자
+*/

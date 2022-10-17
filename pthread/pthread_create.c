e623aa91f51a991a3202eba2b05a73b960bae176
hyunseo <80755341+hyunseo1206@users.noreply.github.com>
2022-10-17 12:11:56 +0900

    Update pthread_create.c

--- pthread/pthread_create.c
+++ pthread/pthread_create.c
@@ -1,8 +1,8 @@
+
 int pthread_create(pthread_t * restrict thread,
 		const pthread_attr_t *restrict attr,
 		void *(*start_routine)(void *),
 		void *restrict arg);
-    
     
 /*    
 pthread_t *restrict thread
